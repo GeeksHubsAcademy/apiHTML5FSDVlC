4c8f4f3d0f9f2efee61923bc38fbb90a242b2b85
Dave86dev <davidochando1986@gmail.com>
2022-02-03 13:08:55 +0100

    drag and drop implementado

--- js/main.js
+++ js/main.js
@@ -1,36 +1,53 @@
 
 
-//localStorage
+// //localStorage
 
-let diaSemana = "Jueves";
+// let diaSemana = "Jueves";
 
-localStorage.setItem('diaSemanal', diaSemana);
+// localStorage.setItem('diaSemanal', diaSemana);
 
-console.log(localStorage.getItem('diaSemanal'));
+// console.log(localStorage.getItem('diaSemanal'));
 
 
-let navegacionAngel = {
-    articulos : {
-        articulo1 : "Portátil Lenovo",
-        articulo2 : "Ratón",
-        articulo3 : "Monitor"
-    },
-    busquedas : 5
+// let navegacionAngel = {
+//     articulos : {
+//         articulo1 : "Portátil Lenovo",
+//         articulo2 : "Ratón",
+//         articulo3 : "Monitor"
+//     },
+//     busquedas : 5
+// }
+
+
+// localStorage.setItem('datosAngel', JSON.stringify(navegacionAngel));
+
+
+// let resultadoDatosAngel = JSON.parse(localStorage.getItem('datosAngel'));
+
+// console.log(resultadoDatosAngel);
+
+
+// //limpiando un elemento en concreto del localStorage
+
+// localStorage.removeItem('datosAngel');
+
+// //limpio la app localStorage por COMPLETO
+
+// localStorage.clear();
+
+
+//DRAG & DROP
+
+const drag = (ev) => {
+    ev.dataTransfer.setData("text", ev.target.id);
+};
+
+const allowDrop = (ev) => {
+    ev.preventDefault();
 }
 
-
-localStorage.setItem('datosAngel', JSON.stringify(navegacionAngel));
-
-
-let resultadoDatosAngel = JSON.parse(localStorage.getItem('datosAngel'));
-
-console.log(resultadoDatosAngel);
-
-
-//limpiando un elemento en concreto del localStorage
-
-localStorage.removeItem('datosAngel');
-
-//limpio la app localStorage por COMPLETO
-
-localStorage.clear();
+const drop = (ev) => {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
